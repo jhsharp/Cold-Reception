ba1e5ac4a7491b1579b0398e88245c3d5e45123f
jhsharp <73131537+jhsharp@users.noreply.github.com>
2022-01-02 21:29:21 -0600

    Animations (Idle, Walking, and Jumping)

--- WinterGameUnity/Assets/Scripts/PlayerController.cs
+++ WinterGameUnity/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     private PlayerControls controls;
     private Rigidbody2D rb;
     private Collider2D col;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
     [SerializeField] private LayerMask ground;
 
     private float moveInput;
@@ -19,6 +21,8 @@ public class PlayerController : MonoBehaviour
         controls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -38,12 +42,27 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Movement
+        move();
+        if (isGrounded()) animator.SetBool("Grounded", true);
+        else animator.SetBool("Grounded", false);
+    }
+
+    private void move()
+    {
+        // Read input
         moveInput = controls.Player.Move.ReadValue<float>();
+        // Move the player
         moveChange = Vector3.zero;
         moveChange.x = moveInput * moveSpeed * Time.deltaTime;
         transform.position += moveChange;
+        // Check for wall collisions
         if (collideWalls()) transform.position -= moveChange;
+        // Animation
+        if (moveInput != 0) animator.SetBool("Walk", true);
+        else animator.SetBool("Walk", false);
+        // Sprite Flip
+        if (moveInput < 0) spriteRenderer.flipX = true;
+        else if (moveInput > 0) spriteRenderer.flipX = false;
     }
 
     private void jump()
@@ -51,6 +70,7 @@ public class PlayerController : MonoBehaviour
         if (isGrounded())
         {
             rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+            animator.SetTrigger("Jump");
         }
     }
 
