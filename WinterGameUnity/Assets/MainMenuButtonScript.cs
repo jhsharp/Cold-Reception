69ec48793b7d30163f9f9da3f19c9c23fcf63651
jhsharp <73131537+jhsharp@users.noreply.github.com>
2022-02-20 19:12:31 -0600

    Level 1 Music/Main Menu Fix

--- WinterGameUnity/Assets/MainMenuButtonScript.cs
+++ WinterGameUnity/Assets/MainMenuButtonScript.cs
@@ -8,7 +8,7 @@ public class MainMenuButtonScript : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Level-1");
     }
 
     public void ExitGame()
